dfaf2df6e166eb90fdc5326384418612f0def51d
Miguel <magasuan@gmail.com>
2019-01-21 16:01:35 +0100

    Realizados test 4 y 5

--- src/test/java/es/upm/grise/profundizacion2018/examenFinal/GreetingTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/examenFinal/GreetingTest.java
@@ -2,7 +2,10 @@ package es.upm.grise.profundizacion2018.examenFinal;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Calendar;
@@ -14,20 +17,10 @@ import org.junit.Test;
 public class GreetingTest {
 	//No hace falta MyCalendar
 //	public abstract class MyCalendar extends Calendar {
-//		//MyCalendar calendar = (MyCalendar) MyCalendar.getInstance();
-//		private int hora;
-//		public MyCalendar(int hora) {
-//			hora = hora;
-//		}
-//		public int getHora() {
-//			return hora;
-//		}
 //	}
 	private final Greeting greeting = spy(new Greeting());
 	@Test
 	public void smokeTest1() {
-		//MyCalendar calendario = mock(MyCalendar.class);
-		//when(calendario.getHora()).thenReturn(3);
 		when(this.greeting.getHora()).thenReturn(8);
 		assertEquals("Good morning", greeting.getGreeting(null));
 	}
@@ -54,11 +47,17 @@ public class GreetingTest {
 		assertEquals("Buenas tardes", greeting.getGreeting(Language.SPANISH));
 	}
 
-//	@Test
-//	public void smokeTest4() {
-//		MyCalendar calendario = mock(MyCalendar.class);
-//		when(calendario.getHora()).thenReturn(3);
-//		Greeting greeting = new Greeting();
-//		assertEquals("Good morning", greeting.getGreeting(null));
-//	}
+	@Test
+	public void smokeTest4() {
+		this.greeting.getGreeting(null);
+		verify(this.greeting,times(1)).getDefaultLanguage();
+		verify(this.greeting,times(1)).getMessage(TimeOfTheDay.AFTERNOON, Language.ENGLISH);
+	}
+	
+	@Test
+	public void smokeTest5() {		
+		greeting.getGreeting(Language.SPANISH);
+		verify(this.greeting,never()).getDefaultLanguage();
+		verify(this.greeting,times(1)).getMessage(TimeOfTheDay.AFTERNOON, Language.SPANISH);
+	}
 }
